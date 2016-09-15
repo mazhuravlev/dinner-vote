2e5994e28e80f994a44f79e7ecb0a10fb7f05b78
Mikhail Zhuravlev <m.zhuravlev61@gmail.com>
2016-09-15 21:25:40 +0300

    removed and ignored build directory

--- build/routing.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var router_1 = require("@angular/router");
-var index_component_1 = require("./components/index/index.component");
-var appRoutes = [
-    {path: '', redirectTo: '/index', pathMatch: 'full'},
-    {path: 'index', component: index_component_1.IndexComponent}
-];
-exports.routing = router_1.RouterModule.forRoot(appRoutes);
-//# sourceMappingURL=routing.js.map
